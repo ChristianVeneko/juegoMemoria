5e363db7c272657bda40d51ea194bcd143349853
Christian Mendoza <christianmendozater@gmail.com>
2022-09-04 06:03:01 -0400

    boton 2

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let tarjetasDestapadas = 0;
 let tarjeta1 = null;
 let tarjeta2 = null;
 let primerResultado = null;
+let segundoResultado = null;
 
 let numeros = [1,1,2,2,3,3,4,4,5,6,6,7,7,8,8];
 
@@ -22,6 +23,15 @@ function destapar(id){
 
         // Deshabilitar primer boton
         tarjeta1.disabled = true;
+    }else id(tarjetasDestapadas == 2)
+    {
+        //mostrar segundo numero
+        tarjeta2 = document.getElementById(id);
+        segundoResultado = numeros[id];
+        tarjeta2.innerHTML = segundoResultado;
+
+        // Deshabilitar segundo boton
+        tarjeta2.disabled = true;
     }
 
 }
