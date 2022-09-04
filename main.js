0e83a93a92f1b199b805eabbd384c6d7255b61c6
Christian Mendoza <christianmendozater@gmail.com>
2022-09-04 06:08:20 -0400

    solucion de error escrito en else if, y se agrego el incremento de movimientos en estadisticas

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ let tarjeta1 = null;
 let tarjeta2 = null;
 let primerResultado = null;
 let segundoResultado = null;
+let movimientos = 0;
+
+//apuntando a documento html
+let mostrarMovimientos = document.getElementById('movimientos');
 
 let numeros = [1,1,2,2,3,3,4,4,5,6,6,7,7,8,8];
 
@@ -23,7 +27,7 @@ function destapar(id){
 
         // Deshabilitar primer boton
         tarjeta1.disabled = true;
-    }else id(tarjetasDestapadas == 2)
+    }else if(tarjetasDestapadas == 2)
     {
         //mostrar segundo numero
         tarjeta2 = document.getElementById(id);
@@ -32,6 +36,10 @@ function destapar(id){
 
         // Deshabilitar segundo boton
         tarjeta2.disabled = true;
+
+        //incrementar movimientos
+        movimientos++;
+        mostrarMovimientos.innerHTML = `Movimientos: ${movimientos}`;
     }
 
 }
